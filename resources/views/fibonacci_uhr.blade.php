5dcf4cacc77e785beb0a5bf400567a6f2ca74611
andrii <andrii.romanyshyn@jetsoftpro.com>
2024-05-29 17:08:20 +0200

    feat(Fibonacci): time reload

--- resources/views/fibonacci_uhr.blade.php
+++ resources/views/fibonacci_uhr.blade.php
@@ -1,7 +1,9 @@
-<!DOCTYPE html>
+\
+
+        <!DOCTYPE html>
 <html>
 <head>
-    <title>Fibonacci Uhr</title>
+    <title>Fibonacci Clock</title>
     <style>
         .square {
             display: inline-block;
@@ -10,11 +12,16 @@
     </style>
 </head>
 <body>
-<h1>Aktuelle Zeit: {{ $time['hours'] }}:{{ str_pad($time['minutes'], 2, '0', STR_PAD_LEFT) }}</h1>
+<h1>Current Time: {{ $time['hours'] }}:{{ str_pad($time['minutes'], 2, '0', STR_PAD_LEFT) }}</h1>
 <div style="display: flex; flex-wrap: wrap; width: 200px;">
     @foreach ([1, 1, 2, 3, 5] as $square)
         <div class="square" style="width: {{ $square * 20 }}px; height: {{ $square * 20 }}px; background-color: {{ $representation[$square] }};"></div>
     @endforeach
 </div>
+<script>
+    setInterval(function() {
+        location.reload();
+    }, 300000);
+</script>
 </body>
 </html>
